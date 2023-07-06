c3ca549eb02c4946ed9ff4d84bfe0b35d3337641
chordfish <592229466@qq.com>
2023-07-06 10:53:29 +0800

    day4

---
+++ src/com/tedu/element/map/MapObj.java
@@ -0,0 +1,12 @@
+package com.tedu.element.map;
+
+import com.tedu.element.ElementObj;
+
+import java.awt.*;
+
+public class MapObj extends ElementObj {
+    @Override
+    public void showElement(Graphics g) {
+
+    }
+}
--- src/com/tedu/manager/GameLoad.java
+++ src/com/tedu/manager/GameLoad.java
@@ -3,9 +3,18 @@ package com.tedu.manager;
 import com.tedu.controller.Direction;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
+/**
+ * 加载器，读取配置文件的工具类
+ * 提供一系列static方法
+ * @author LSR
+ */
 public class GameLoad {
     public static Map<Direction, ImageIcon> imgMap = null;
 
@@ -15,5 +24,34 @@ public class GameLoad {
         imgMap.put(Direction.RIGHT, new ImageIcon("image/tank/play1/player1_right.png"));
         imgMap.put(Direction.UP, new ImageIcon("image/tank/play1/player1_up.png"));
         imgMap.put(Direction.DOWN, new ImageIcon("image/tank/play1/player1_down.png"));
+    }
+
+    // 读取配置文件的类
+    private static Properties pro = new Properties();
+
+    /**
+     * 传入地图id加载地图
+     * @param mapId 地图文件id
+     */
+    public static void MapLoad(int mapId) {
+        String mapName = "com/tedu/text/" + mapId + ".map";
+        // 用io流获取文件对象，加载包内文件
+        ClassLoader classLoader = GameLoad.class.getClassLoader(); // 类加载器
+        InputStream maps = classLoader.getResourceAsStream(mapName);
+        if (maps == null) {
+            System.out.println("配置文件读取异常，清重新安装");
+            return;
+        }
+        try {
+            pro.load(maps);
+            Enumeration<?> names = pro.propertyNames();
+            System.out.println(names);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MapLoad(5);
     }
 }
